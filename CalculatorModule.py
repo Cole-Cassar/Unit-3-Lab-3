7ce021a13b0f4cffefddf7071f5da39c17915278
Cole <Cole@example.com>
2023-11-09 20:29:55 -0700

    Calculator module

---
+++ CalculatorModule.py
@@ -0,0 +1,12 @@
+def add(first_number,second_number):
+    result = first_number + second_number
+    return result
+def subtract(first_number, second_number):
+    result = first_number - second_number
+    return result
+def multiply(first_number, second_number):
+    result = first_number * second_number
+    return result
+def divide(first_number, second_number):
+    result = first_number / second_number
+    return result
